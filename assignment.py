22d4a2cb767aa94535de32b58e0c31a0c0984c34
Anand Balaram <anandbalaram@cmail.carleton.ca>
2024-11-26 12:27:08 -0500

    Start to add more documentation. Start to make it so that system reprompts user if input is invalid

--- assignment.py
+++ assignment.py
@@ -10,6 +10,20 @@ import subprocess
 
 import sys
 
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+print("Setting up and installing libaries...")
+print()
+try:
+    import keyboard
+    print("All dependencies already installed!")
+except ImportError:
+    print("keyboard library not found. Installing...")
+    install("keyboard")
+    import keyboard  # Retry importing after installation
+print()
+
 
 # Constants
 
@@ -52,32 +66,30 @@ HASH_POSITION_IN_FILE = PASSWORD_FILE_STRUCTURE.index("hash")
 
 SALT_POSITION_IN_FILE = PASSWORD_FILE_STRUCTURE.index("salt")
 
+INVALID_INPUT = "Invalid input!"
+
 
 # Functions
 
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-def setup_libraries():
-    print("Setting up and installing libaries...")
-    print()
-    try:
-        import keyboard
-        print("All dependencies already installed!")
-    except ImportError:
-        print("keyboard library not found. Installing...")
-        install("keyboard")
-        import keyboard  # Retry importing after installation
-    print()
-
-def set_time():
-    time = int(input("Please enter the hour of the day (use 24 hour notation): "))
-    return accessible_to_teller(time)
+def set_time()->bool:
+    """ Has the user set the time to a value between 0-24. Returns whether or not teller has access"""
+    while True:
+        try:
+            time = int(input("Please enter the hour of the day (use 24 hour notation): "))
+            if time < 0 or time > 24:
+                print(INVALID_INPUT)
+                continue
+            return accessible_to_teller(time)
+        except:
+            print(INVALID_INPUT)
+            continue
 
 def accessible_to_teller(time):
+    """ Returns whether or not teller has access based on the given time"""
     return time >= 9 and time <=17
 
 def startup():
+    """ Prints the startup menu for the user to see"""
     print("justInvestSystem")
     print("----------------------------------")
 
@@ -90,6 +102,7 @@ def print_operations():
     print("5. View Financial Planner contact info")
     print("6. View money market instruments")
     print("7. View private consumer instruments")
+    print("0. Quit")
     print()
 
 def user_sign_in(teller_access):
@@ -99,13 +112,13 @@ def user_sign_in(teller_access):
         print()
         username = input("Enter username to sign in or enter 0 to register: ")
         print()
-        quit = False
+        signup = False
         try:
-            quit = int(username) == 0
+            signup = int(username) == 0
         except:
-            quit = False
-        if quit:
-            launch_signup()
+            signup = False
+        if signup:
+            return launch_signup()
         elif valid_username(username):
             if (USERS[username] == "Teller" and not teller_access):
                 print("Teller can't access the system outside of business hours!")
@@ -118,7 +131,28 @@ def user_sign_in(teller_access):
     
 def launch_signup():
     username = input("Please enter your desired username: ")
-    password = input("Please enter your desired password: ")
+    password = input_password()
+    print("Signup was successful!")
+    return username
+
+def input_password():
+    prompt="Enter password: "
+    print(prompt, end='', flush=True)
+    password = ""
+    while True:
+        key = keyboard.read_event(suppress=True)
+        if key.event_type == "down":
+            if key.name == "enter":
+                print()
+                break
+            elif key.name == "backspace":
+                if password:
+                    password = password[:-1]
+                    print("\b \b", end='', flush=True)
+            elif len(key.name) == 1:
+                password += key.name
+                print("*", end='', flush=True)
+    return password
 
 def valid_username(username):
     return username in USERS
@@ -167,7 +201,6 @@ def access_control(user_select, user_role):
     print("Access Denied!")
 
 def main():
-    setup_libraries()
     teller_access = set_time()
     print_operations()
     user_role = user_sign_in(teller_access)
